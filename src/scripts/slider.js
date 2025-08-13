014d92e16636d9a404c6074ce596f5a1132f3fde
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-08-13 16:44:46 +0300

    переподключение слайдера

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".sound__slider", {
+/*var swiper = new Swiper(".sound__slider", {
   loop: true,
   slidesPerView: 5,
   spaceBetween: 65,
@@ -38,4 +38,56 @@ var swiper1 = new Swiper(".comments__slider", {
     prevEl: ".comments-button-prev",
     nextEl: ".comments-button-next",
   },
+});
+*/ // slider.js
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules"; // Импортируйте Navigation из swiper/modules
+import "swiper/css";
+import "swiper/css/navigation"; // Подключите стили для Navigation
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Инициализация первого слайдера
+  const swiper = new Swiper(".sound__slider", {
+    modules: [Navigation], // Укажите Navigation как модуль
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 65,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      400: {
+        slidesPerView: 1.5,
+      },
+      425: {
+        slidesPerView: 2,
+      },
+      1300: {
+        slidesPerView: 3,
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+      1401: {
+        slidesPerView: 5,
+        spaceBetween: 65,
+      },
+    },
+    navigation: {
+      prevEl: ".cardsSlider-button-prev",
+      nextEl: ".cardsSlider-button-next",
+    },
+  });
+
+  // Инициализация второго слайдера
+  const swiper1 = new Swiper(".comments__slider", {
+    modules: [Navigation], // Укажите Navigation как модуль
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      prevEl: ".comments-button-prev",
+      nextEl: ".comments-button-next",
+    },
+  });
 });
