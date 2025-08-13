5cb61bba2f85f34e5b2258b23cdbc70109e1fb4d
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-08-13 17:52:22 +0300

    переменные

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const TEL = "8 (800) 770-04-73";
+  const TELLINK = "+78007700473";
+
   Promise.all([
     fetch("/header.html").then((res) => res.text()),
     fetch("/footer.html").then((res) => res.text()),
@@ -7,8 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const headerEl = document.getElementById("header-placeholder");
       const footerEl = document.getElementById("footer-placeholder");
 
+      // Insert header and footer
       if (headerEl) headerEl.innerHTML = header;
       if (footerEl) footerEl.innerHTML = footer;
+      const phoneLinkEl = document.getElementById("phoneLink");
+      if (phoneLinkEl) {
+        phoneLinkEl.textContent = TEL;
+        phoneLinkEl.href = `tel:${TELLINK}`;
+      } else {
+        console.error("Phone link element not found");
+      }
     })
     .catch((error) => {
       console.error("Error loading templates:", error);
